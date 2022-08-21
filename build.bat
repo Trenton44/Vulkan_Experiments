beb74fdac60faa097d7296e72d3327e795fe798b
Trenton Choate <38508613+Trenton44@users.noreply.github.com>
2022-08-21 17:49:05 -0500

    -specified path to glfw include folder in includePath -cleaned up build.bat

--- build.bat
+++ build.bat
@@ -1,16 +1,11 @@
 set compiler_path="C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
-vulkan
 call %compiler_path%
 
-::sets output file of .obj object created by compiler
-set file_object_path="bin/obj/"
-
-set includes=/I "/src" /I "%VULKAN_SDK%/Include" 
-echo "PATH: " %LIBPATH%
-set links=/LIBPATH:%VULKAN_SDK%/Lib vulkan-1.lib /out:"bin/main.exe"
+set includes=/I /src /I %VULKAN_SDK%/Include /I /libs/glfw-3.3.8.bin.WIN64/include
 set define=/D DEBUG
+set links=/LIBPATH:%VULKAN_SDK%/Lib vulkan-1.lib /out:"bin/main.exe"
 
 echo "Building main..."
 ::arguments come from microsoft webpage https://docs.microsoft.com/en-us/cpp/build/reference/compiler-command-line-syntax?view=msvc-170
-::this is the compiler call. call %compiler-path% above lets us call this
-cl /EHsc /Fo"%file_object_path%" %includes% %defines% src/main.cpp /link %links% 
+::this is the compiler call. call %compiler-path% above lets us call this, as it makes this script run from the Developer Command Prompt for VSCode 2022
+cl /EHsc /Fo"bin/obj/" %includes% %defines% src/main.cpp /link %links% 
